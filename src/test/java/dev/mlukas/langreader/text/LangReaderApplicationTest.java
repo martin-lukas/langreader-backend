11b01d0fef98c81dba05ee1a0cf8b80331b1365a
martin-lukas <martinlukas24@protonmail.com>
2022-05-27 16:01:11 +0200

    Remove Spring security test deps

--- src/test/java/dev/mlukas/langreader/text/LangReaderApplicationTest.java
+++ src/test/java/dev/mlukas/langreader/text/LangReaderApplicationTest.java
@@ -3,12 +3,10 @@ package dev.mlukas.langreader.text;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class LangReaderApplicationTest {
     private final ApplicationContext context;
 
